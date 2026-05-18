8f4d40741cf4ecaf416cd76b742f8ebfec4318e7
RockmellLoyo <rockmellloyo@gmail.com>
2026-05-18 11:03:23 -0400

    Llave de Telegram corregida con mayusculas reales

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import * as cheerio from 'cheerio';
 import https from 'https';
 
 const FIREBASE_URL = "https://control-data-rockmell-default-rtdb.firebaseio.com";
-const TELEGRAM_TOKEN = "8819201042:aahmQbrjyBxYwmhd6qECdP92VM0BYsmi96e";
+const TELEGRAM_TOKEN = "8819201042:AAHMqBRJYbXyWMHD6QecDp92vm0bySMI96E";
 const TELEGRAM_CHAT_ID = "5844630655";
 const agent = new https.Agent({ rejectUnauthorized: false });
 
